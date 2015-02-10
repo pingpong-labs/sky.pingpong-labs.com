31d2e9a612ee05322735277a449bb2a45544d900
gravitano <gravitano16@gmail.com>
2015-02-10 13:10:36 +0700

    Update

--- app/Http/Controllers/DocsController.php
+++ app/Http/Controllers/DocsController.php
@@ -65,7 +65,7 @@ class DocsController extends Controller {
 	 */
 	protected function isVersion($version)
 	{
-		return in_array($version, ['master', '5.0', '4.2', '4.1', '4.0']);
+		return in_array($version, ['master']);
 	}
 
 	/**
@@ -77,10 +77,6 @@ class DocsController extends Controller {
 	{
 		return [
 			'master' => 'Master',
-			'5.0' => '5.0',
-			'4.2' => '4.2',
-			'4.1' => '4.1',
-			'4.0' => '4.0',
 		];
 	}
 
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Laravel - The PHP Framework For Web Artisans</title>
+	<title>Sky - The Extra Laravel Packages</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="author" content="Taylor Otwell">
-	<meta name="description" content="Laravel - The PHP framework for web artisans.">
+	<meta name="author" content="Gravitano">
+	<meta name="description" content="Sky - The Extra Laravel Packages">
 	<meta name="keywords" content="laravel, php, framework, web, artisans, taylor otwell">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--[if lte IE 9]>
@@ -45,19 +45,11 @@
 		<ul>
 			@include('partials.main-nav')
 		</ul>
-		<p>Laravel is a trademark of Taylor Otwell. Copyright &copy; Taylor Otwell.</p>
+		<p>Sky is a trademark of Gravitano. Copyright &copy; Gravitano.</p>
 		<p class="less-significant"><a href="http://jackmcdade.com">Design by Jack McDade</a></p>
 	</footer>
 	<script src="/assets/js/laravel.js"></script>
 	<script src="/assets/js/viewport-units-buggyfill.js"></script>
 	<script>window.viewportUnitsBuggyfill.init();</script>
-    <script>
-        var _gaq=[['_setAccount','UA-23865777-1'],['_trackPageview']];
-        (function(d,t){
-            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)
-        }(document,'script'));
-    </script>
 </body>
 </html>
