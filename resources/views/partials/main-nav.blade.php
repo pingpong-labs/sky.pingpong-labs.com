a904f3b706421f3817495c9732377795f0de6f00
gravitano <gravitano16@gmail.com>
2015-02-10 13:17:58 +0700

    udpate main nav

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,25 +1,10 @@
+<li class="nav-docs"><a href="/">Home</a></li>
 <li class="nav-docs"><a href="/docs">Documentation</a></li>
-<li class="nav-laracasts"><a href="https://laracasts.com">Laracasts</a></li>
-<li class="nav-forge"><a href="https://forge.laravel.com">Forge</a></li>
-<li class="nav-api"><a href="/api/5.0">API</a></li>
-<li class="dropdown community-dropdown">
-	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Conference <span class="caret"></span></a>
-	<ul class="dropdown-menu" role="menu">
-		<li><a href="http://laracon.us">Laracon US</a></li>
-		<li><a href="http://laracon.eu">Laracon EU</a></li>
-	</ul>
-</li>
 <li class="dropdown community-dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Community <span class="caret"></span></a>
-	<ul class="dropdown-menu" role="menu">
-		<li><a href="https://github.com/laravel/laravel">Github</a></li>
-		<li><a href="https://twitter.com/laravelphp">Twitter</a></li>
-		<li class="divider"></li>
-		<li><a href="https://laracasts.com/discuss">Laracasts Forums</a></li>
-		<li><a href="http://laravel.io/forum">Laravel.io Forums</a></li>
-		<li><a href="http://larachat.co">LaraChat Slack Channel</a></li>
-		<li><a href="https://laravel-news.com/">Laravel News</a></li>
-		<li class="divider"></li>
-		<li><a href="https://larajobs.com/?partner=5#/">LaraJobs</a></li>
+	<ul class="dropdown-menu pull-right" role="menu">
+		<li><a href="https://github.com/pingpong-labs/sky">Github</a></li>
+		<li><a href="https://twitter.com/pingponglabs">Twitter</a></li>
+		<li><a href="https://www.facebook.com/pingponglabs">Facebook</a></li>
 	</ul>
 </li>
