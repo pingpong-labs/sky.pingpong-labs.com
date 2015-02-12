a586de6b52f7415c1b061a875fa3340ff5307ccc
gravitano <gravitano16@gmail.com>
2015-02-12 20:19:23 +0700

    Remove docs script

--- build/docs.sh
+++ build/docs.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-
-DEPLOYDIR=/var/www/skypingpong-labscom
-
-cd $DEPLOYDIR/resources/docs/master && git pull origin master
-cd $DEPLOYDIR/resources/docs/1.0 && git pull origin 1.0
-cd $DEPLOYDIR/resources/docs/2.0 && git pull origin 2.0
