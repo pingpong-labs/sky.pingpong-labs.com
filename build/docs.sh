d6c9778942983d52ef70b96bcb775134d04d79e9
gravitano <gravitano16@gmail.com>
2015-02-12 20:15:43 +0700

    Update docs shell script

--- build/docs.sh
+++ build/docs.sh
@@ -2,4 +2,6 @@
 
 DEPLOYDIR=/var/www/skypingponglabscom
 
-cd $DEPLOYDIR/resources/docs/master && git pull origin 2.0
+cd $DEPLOYDIR/resources/docs/master && git pull origin master
+cd $DEPLOYDIR/resources/docs/1.0 && git pull origin 1.0
+cd $DEPLOYDIR/resources/docs/2.0 && git pull origin 2.0
