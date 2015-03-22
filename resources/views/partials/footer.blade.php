34130bc8e02fbd00cf29195c54be525cee2ec5c6
Gravitano <gravitano16@gmail.com>
2015-03-23 02:15:40 +0700

    Update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,27 +2,25 @@
   <div class="{!! Request::is('/') ? 'container' : '' !!}">
     <div class="row">
       <div class="col l6 s12">
-        <h5 class="white-text">About Pingpong Labs</h5>
+        <h5 class="white-text">About</h5>
         <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
 
 
       </div>
       <div class="col l3 s12">
-        <h5 class="white-text">Settings</h5>
+        <h5 class="white-text">Version</h5>
         <ul>
-          <li><a class="white-text" href="#!">Link 1</a></li>
-          <li><a class="white-text" href="#!">Link 2</a></li>
-          <li><a class="white-text" href="#!">Link 3</a></li>
-          <li><a class="white-text" href="#!">Link 4</a></li>
+          <li><a class="white-text" href="/docs/master">Develop</a></li>
+          <li><a class="white-text" href="/docs/1.0">1.0</a></li>
+          <li><a class="white-text" href="/docs/2.0">2.0</a></li>
         </ul>
       </div>
       <div class="col l3 s12">
         <h5 class="white-text">Connect</h5>
         <ul>
-          <li><a class="white-text" href="#!">Link 1</a></li>
-          <li><a class="white-text" href="#!">Link 2</a></li>
-          <li><a class="white-text" href="#!">Link 3</a></li>
-          <li><a class="white-text" href="#!">Link 4</a></li>
+          <li><a class="white-text" href="https://github.com/pingpong-labs">Github</a></li>
+          <li><a class="white-text" href="https://twitter.com/pingponglabs">Twitter</a></li>
+          <li><a class="white-text" href="https://www.facebook.com/pingponglabs">Facebook</a></li>
         </ul>
       </div>
     </div>
