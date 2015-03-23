4f0021dff90e729a4f6299f45935c8446b98221b
Gravitano <gravitano16@gmail.com>
2015-03-23 14:16:07 +0700

    Hide 'Pingpong' on small device

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
   <nav class="blue darken-1">
     <div class="nav-wrapper {!! Request::is('docs/*') ? '' : 'container' !!}"> 
-      <a href="#" class="brand-logo">Pingpong Sky</a>
+      <a href="#" class="brand-logo"><span class="hide-on-small-only">Pingpong</span> Sky</a>
       @include('partials.main-nav')
     </div>
   </nav>
