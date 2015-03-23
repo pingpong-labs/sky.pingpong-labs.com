c48464286b175a894f1097e4135c3ac1a0308ec9
Gravitano <gravitano16@gmail.com>
2015-03-23 14:14:47 +0700

    Add google site verification & optimize some meta tags

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,9 +1,14 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta name="google-site-verification" content="D2ZedtfXH-qgzRPrWLhBUAOctj-X264NbFFhAtiLbgk" />
 <meta name="description" content="Pingpong Sky is an extra laravel package that created for Laravel 4 and Laravel 5.">
 <meta name="author" content="Pingpong Labs">
 <meta name="keywords" content="pingpong sky, laravel, pingpong labs, gravitano, sky">
 
 <!--Import materialize.css-->
 <link type="text/css" rel="stylesheet" href="/assets/materialize/dist/css/materialize.min.css"  media="screen,projection"/>
-<!--Let browser know website is optimized for mobile-->
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 <link rel="stylesheet" type="text/css" href="/assets/font-awesome/css/font-awesome.css">
+<!--[if lte IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
