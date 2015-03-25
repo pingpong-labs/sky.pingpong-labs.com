dcb76950127bba0979e86349e7f6aa93fe917044
Gravitano <gravitano16@gmail.com>
2015-03-25 21:49:28 +0700

    Update site description

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 <meta name="google-site-verification" content="D2ZedtfXH-qgzRPrWLhBUAOctj-X264NbFFhAtiLbgk" />
-<meta name="description" content="Pingpong Sky is an extra laravel package that created for Laravel 4 and Laravel 5.">
+<meta name="description" content="Pingpong Sky is a PHP component made specifically for Laravel 4 and Laravel 5.">
 <meta name="author" content="Pingpong Labs">
 <meta name="keywords" content="pingpong sky, laravel, pingpong labs, gravitano, sky, technology, documentation, hosting">
 
