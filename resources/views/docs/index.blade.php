c24fbf6d97ac8c8745b59fde8057d7025966cee9
Gravitano <gravitano16@gmail.com>
2015-04-05 23:10:36 +0700

    update page title of doc page

--- resources/views/docs/index.blade.php
+++ resources/views/docs/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Pingpong Sky - An Extra Laravel Component</title>
+	<title>Pingpong Sky - Documentation</title>
   @include('partials.head')
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.css">
   <link rel="stylesheet" type="text/css" href="/css/docs.css">
