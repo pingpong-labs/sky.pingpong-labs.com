398f5f366d12706674e3e549f36dc31b4f51ce52
Gravitano <gravitano16@gmail.com>
2015-05-09 00:35:00 +0700

    Fix ad position

--- resources/views/docs/index.blade.php
+++ resources/views/docs/index.blade.php
@@ -14,10 +14,12 @@
       <div class="col s12">
         {!! $content !!}
       </div>
-      <!-- Begin: KlikSaya.com -->
-      <script src="http://scr.kliksaya.com/js-ad.php?zid=171065" type="text/javascript">
-      </script>
-      <!-- End: KlikSaya.com -->
+      <div class="ad center">
+        <!-- Begin: KlikSaya.com -->
+        <script src="http://scr.kliksaya.com/js-ad.php?zid=171065" type="text/javascript">
+        </script>
+        <!-- End: KlikSaya.com -->
+      </div>
 
       </div>
     </section>
