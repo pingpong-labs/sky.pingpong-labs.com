3d5e167e68c29ac6dcd9fcf3b51f1e1c491733ab
Gravitano <gravitano16@gmail.com>
2015-05-27 13:59:06 +0700

    Added scroll to top button

--- resources/views/docs/index.blade.php
+++ resources/views/docs/index.blade.php
@@ -13,6 +13,14 @@
     <section class="row">
       <div class="col s12">
         {!! $content !!}
+
+        {{-- button --}}
+
+        <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+         <a style="display: none;" class="btn-floating scrollTop btn-large waves-effect waves-light blue">
+          <i class="mdi-hardware-keyboard-arrow-up"></i>
+          </a>
+         </div>
       </div>
       <div class="ad center">
         <!-- Begin: KlikSaya.com -->
@@ -29,6 +37,20 @@
   $('pre').addClass('prettyprint');
   </script>
   <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
+  <script>
+    /* scroll top */
+    $(window).scroll(function(){
+      if ($(window).scrollTop() > 100) {
+        $('.scrollTop').show();
+      }else{
+        $('.scrollTop').hide();
+      }
+    });
+
+    $('.scrollTop').on('click', function(){
+      $("html, body").animate({ scrollTop: 0 });
+    });
+  </script>
 
 </body>
 </html>
