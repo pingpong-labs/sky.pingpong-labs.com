cfa6f0b4d620b154a00ae1b240922481c3f9c9d2
Gravitano <gravitano16@gmail.com>
2015-06-08 14:24:46 +0700

    enable cache for production server

--- app/Documentation.php
+++ app/Documentation.php
@@ -1,70 +1,109 @@
-<?php namespace App;
+<?php namespace app;
 
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Contracts\Cache\Repository as Cache;
 
-class Documentation {
+class Documentation
+{
 
-	/**
-	 * The filesystem implementation.
-	 *
-	 * @var Filesystem
-	 */
-	protected $files;
+    /**
+     * The filesystem implementation.
+     *
+     * @var Filesystem
+     */
+    protected $files;
 
-	/**
-	 * The cache implementation.
-	 *
-	 * @var Cache
-	 */
-	protected $cache;
+    /**
+     * The cache implementation.
+     *
+     * @var Cache
+     */
+    protected $cache;
 
-	/**
-	 * Create a new documentation instance.
-	 *
-	 * @param  Filesystem  $files
-	 * @param  Cache  $cache
-	 * @return void
-	 */
-	public function __construct(Filesystem $files, Cache $cache)
-	{
-		$this->files = $files;
-		$this->cache = $cache;
-	}
+    /**
+     * The cache lifetime.
+     *
+     * @var integer
+     */
+    protected $lifetime = 1140;
 
-	/**
-	 * Get the documentation index page.
-	 *
-	 * @param  string  $version
-	 * @return string
-	 */
-	public function getIndex($version)
-	{
-		// return $this->cache->remember('docs.'.$version.'.index', 0, function() use ($version) {
-			$path = base_path('resources/docs/'.$version.'/documentation.md');
-			if ($this->files->exists($path)) {
-				return markdown($this->files->get($path));
-			}
-			return null;
-		// });
-	}
+    /**
+     * Create a new documentation instance.
+     *
+     * @param  Filesystem  $files
+     * @param  Cache  $cache
+     * @return void
+     */
+    public function __construct(Filesystem $files, Cache $cache)
+    {
+        $this->files = $files;
+        $this->cache = $cache;
+    }
 
-	/**
-	 * Get the given documentation page.
-	 *
-	 * @param  string  $version
-	 * @param  string  $page
-	 * @return string
-	 */
-	public function get($version, $page)
-	{
-		// return $this->cache->remember('docs.'.$version.'.'.$page, 0, function() use ($version, $page) {
-			$path = base_path('resources/docs/'.$version.'/'.$page.'.md');
-			if ($this->files->exists($path)) {
-				return markdown($this->files->get($path));
-			}
-			return null;
-		// });
-	}
+    /**
+     * Determine whether the caching feature is enabled.
+     *
+     * @return boolean
+     */
+    public function useCache()
+    {
+        return ! app()->isLocal();
+    }
 
+    /**
+     * Get file contents from the given path and render it using markdown.
+     *
+     * @param  string $path
+     * @param  string|null $default
+     * @return string|null
+     */
+    public function getContents($path, $default = null)
+    {
+        if ($this->files->exists($path)) {
+            return markdown($this->files->get($path));
+        }
+
+        return $default;
+    }
+
+    /**
+     * Get the documentation index page.
+     *
+     * @param  string  $version
+     * @return string
+     */
+    public function getIndex($version)
+    {
+        $path = base_path('resources/docs/'.$version.'/documentation.md');
+
+        if ($this->useCache()) {
+            return $this->cache->remember('docs.'.$version.'.index', $this->lifetime,
+                function () use ($version, $path) {
+                    return $this->getContents($path);
+                });
+        }
+
+        return $this->getContents($path);
+    }
+
+    /**
+     * Get the given documentation page.
+     *
+     * @param  string  $version
+     * @param  string  $page
+     * @return string
+     */
+    public function get($version, $page)
+    {
+        $path = base_path('resources/docs/'.$version.'/'.$page.'.md');
+
+        if ($this->useCache()) {
+            return $this->cache->remember('docs.'.$version.'.index', $this->lifetime,
+                function () use ($version, $path) {
+                    return $this->getContents($path);
+                });
+        }
+        
+        return $this->getContents($path);
+    }
 }
