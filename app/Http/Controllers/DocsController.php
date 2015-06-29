6dd9aab7ea51bb8e1fc6d8b6f81dc3a8c61b4655
Gravitano <gravitano16@gmail.com>
2015-06-29 13:48:26 +0700

    Added new version 2.1

--- app/Http/Controllers/DocsController.php
+++ app/Http/Controllers/DocsController.php
@@ -89,6 +89,7 @@ class DocsController extends Controller
             'master' => 'Master',
             '1.0' => '1.0',
             '2.0' => '2.0',
+            '2.1' => '2.1',
         ];
     }
 }
